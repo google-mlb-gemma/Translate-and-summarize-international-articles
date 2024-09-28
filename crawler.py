72fe1603c15da01cb963877c4dd5409bbde65f44
unknown <florenshio95@gmail.com>
2024-09-28 17:40:15 +0900

    feat:[2024-09-28] 내용 추가

--- crawler.py
+++ crawler.py
@@ -31,3 +31,43 @@ def extract_articles(links_list: list, title_class: str, paragraph_class: str):
         articles.append(article)
     time.sleep(1)
     return articles
+
+
+def extract_articles_alja(links_list: list, title_class: str, paragraph_class: str):
+    articles = []
+    article_route = False
+    liveblog_route = False
+    for link in links_list:
+        article_response = requests.get(link)
+        article_soup = BeautifulSoup(article_response.text, "html.parser")
+        html_values = article_soup.find_all('link', attrs = {'data-chunk': True})
+
+        for v in html_values:
+            if v.get("data-chunk") == "article-route":
+                print("article-route")
+                article_route = True
+                break
+            elif v.get("data-chunk") == "liveblog-route":
+                print("liveblog-route")
+                liveblog_route = True
+                break
+            else:
+                print("False")
+                return "Error"
+
+        if article_route == True:
+            header_values = article_soup.find_all('header', class_=title_class)
+            title = header_values[0].find_all("h1")[0].get_text(strip=True)
+            article = f"# {title}"
+            
+            p_values = article_soup.find_all('div', class_=paragraph_class)
+            for value in p_values:
+                texts = value.find_all('p')
+                for p in texts:
+                    text = p.get_text()
+                    article += f"\n{text}"
+                articles.append(article)
+            article_route = False
+
+        time.sleep(1)
+    return articles
--- prompts.py
+++ prompts.py
@@ -1,5 +1,5 @@
 SUMMARY_PROMPT = """
-<bos><start_of_turn>user
+<start_of_turn>user
 
 You are a chatbot that thinks in English and answer in Korean for User prompt.
 
@@ -13,11 +13,9 @@ Your reasoning should be based on two rules: Thinking and Answer.
 
 1. Translate the question into English.
 
-2. If you have a mix of languages, think of yourself as a multilingual specialist.
+2. Analyze the intention of the question and decide how to respond.
 
-3. Analyze the intention of the question and decide how to respond.
-
-4. Keep the logical reasoning process in mind.
+3. Keep the logical reasoning process in mind.
 
 **Answer:**
 
@@ -29,16 +27,118 @@ Your reasoning should be based on two rules: Thinking and Answer.
 
 4. Don't repeat the same sentence over and over again.
 
+## User prompt
+
 article: {article}
 
-After analyzing the nature of the document, summarize its contents accurately in Korean.
-Keep your answer to seven sentences or less. If there's not much in the article, you can answer in fewer sentences.
-Keep the formatting of Korean sentences consistent.
+After analyzing the article, summarize its contents accurately in Korean.
+If you answer a question multiple times with similar sentences, it's best to organize them into a single sentence.
+All sentences must be complete and end with a dot (.).
 
-## Example
-Bad case : 이건 샘플 문장입니다. 참고 해야함.
-Good case : 이건 샘플 문장입니다. 참고해 주시길 바랍니다.
-
-If you answer a question multiple times with similar sentences, it's best to organize them into a single sentence.<end_of_turn>
+### Example
+모든 문장은 다음과 같이 완성된 상태로 끝나야 합니다. 중간에 끊어지지 않도록 합니다.<end_of_turn>
 
 <start_of_turn>model\n"""
+
+# =====================================================================================================================================
+
+SUMMARY_CHAIN_PROMPT = """
+<start_of_turn>user
+
+You are a chatbot that thinks in English and answer in Korean for User prompt.
+
+Your task is to summarize the article.
+
+Your reasoning should be based on two rules: Thinking and Answer.
+
+## Format of rules
+
+**Thinking:**
+
+1. Translate the question into English.
+
+2. Analyze the intention of the question and decide how to respond.
+
+3. If you have a mix of languages, think of yourself as a multilingual specialist.
+
+4. Keep the logical reasoning process in mind.
+
+**Answer:**
+
+1. Based on the process of thinking, write down detailed answers in Korean.
+
+2. For proper nouns, such as people's names, print them in English as they are.
+
+3. Adjust expressions to fit the korean linguistic characteristics.
+
+4. Don't repeat the same sentence over and over again.<end_of_turn>
+
+<start_of_turn>model
+당신의 요청을 이해했습니다. Thinking과 Answer 규칙에 근거해서 article을 요약하겠습니다. 저에게 요약해야 할 article을 제공해 주세요.<end_of_turn>
+
+<start_of_turn>user
+
+article: {article}
+
+After analyzing the article, summarize its contents accurately in Korean.
+If you answer a question multiple times with similar sentences, it's best to organize them into a single sentence.
+On the very first line, write the title of article.
+Try to limit the number of sentences in your summary to fifteen or so.
+All sentences must be complete and end with a dot (.).
+
+### Example
+모든 문장은 다음과 같이 완성된 상태로 끝나야 합니다. 중간에 끊어지지 않도록 합니다.<end_of_turn>
+
+<start_of_turn>model\n"""
+
+# =====================================================================================================================================
+
+REFLECTION_TEST = """<start_of_turn>user
+
+You are a chatbot that thinks in English and answer in Korean for User prompt.
+
+You'll receive the original article and an AI's summarization of the article.
+
+The original article is in English, and the AI's summarization is in Korean.
+
+Your task is to complement the AI's summary to create a more complete summary.
+
+Your reasoning should be based on two rules: Thinking and Answer.
+
+## Format of rules
+
+**Thinking:**
+
+1. Translate the question into English.
+
+2. Analyze the intention of the question and decide how to respond.
+
+3. If you have a mix of languages, think of yourself as a multilingual specialist.
+
+4. Keep the logical reasoning process in mind.
+
+**Answer:**
+
+1. Based on the process of thinking, write down detailed answers in Korean.
+
+2. For proper nouns, such as people's names, print them in English as they are.
+
+3. Adjust expressions to fit the korean linguistic characteristics.
+
+4. Don't repeat the same sentence over and over again.<end_of_turn>
+
+<start_of_turn>model
+당신의 요청을 이해했습니다. Thinking과 Answer 규칙에 근거해서 원문 article과 AI의 요약문을 참고해서 더 적절한 요약문을 만들겠습니다. 대답은 오로지 요약문만 말하고 다른 대답은 포함하지 않겠습니다.
+저에게 원문 article과 AI의 요약문을 제공해 주세요.<end_of_turn>
+
+<start_of_turn>user
+## User prompt
+
+1. original article: {article}
+
+2. AI's summary: {ai_result}
+
+Use the original article and the AI's summary to create a better summary in korean.<end_of_turn>
+
+<start_of_turn>model\n
+new summary:"""
