b192d0bca03dfe75426da0124e50023dc18c5458
unknown <florenshio95@gmail.com>
2024-09-28 20:00:28 +0900

    feat:[2024-09-28] bot과 agent를 하나로 합친 브랜치

--- agent.py
+++ agent.py
@@ -8,8 +8,11 @@ from langchain_core.output_parsers import StrOutputParser
 #import sys
 #sys.path.append("/data/aisvc_data/NLP/flo/codes/LLM/gemma_sprint/module")
 from prompts import *
-from langchain_community.llms import LlamaCpp
+from langchain_community.llms.llamacpp import LlamaCpp
+from langchain_community.chat_models import ChatLlamaCpp
 
+with open("crawl_class.yaml") as f:
+    classes = yaml.load_all(f, Loader=yaml.FullLoader)
 
 class Agent(ABC):
     def __init__(self, model_name):
@@ -22,31 +25,56 @@ class Agent(ABC):
 
 
 class Gemma(Agent):
-    def __init__(self, project_name, model_path, n_batch=8192, n_ctx=8192, n_threads=16, temperature=0):
+    def __init__(self, project_name, model_path, max_tokens=512, n_batch=8192, n_ctx=8192, n_threads=16, temperature=0, verbose=False):
         super().__init__(model_name=model_path)
-        os.environ["LANGCHAIN_TRAICING_V2"] = "true"
-        os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-        os.environ["LANGCHAIN_API_KEY"] = "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"
-        os.environ["LANGCHAIN_PROJECT"] = project_name
 
         self.prompt = PromptTemplate.from_template(SUMMARY_PROMPT)
         
-        self.client = LlamaCpp(
+        self.client = ChatLlamaCpp(
             model_path=self.model_name,
+            max_tokens=max_tokens,
             n_threads=n_threads,
             n_batch=n_batch,
             n_ctx=n_ctx,
             n_gpu_layers=-1,
             callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-            verbose=True,
+            verbose=verbose,
             f16_kv=True,
             temperature=temperature,
             top_p=0.9,
             top_k=50
         )
 
-    def summarize(self, article):
+    def summarize(self, article: str):
         chain = self.prompt | self.client | StrOutputParser()
         result = chain.invoke({'article':article})
+
+        return result
+
+    def reflection(self, article: str, ai_result):
+        reflection_prompt = PromptTemplate.from_template(REFLECTION_TEST)
+        chain = reflection_prompt | self.client | StrOutputParser()
+        result = chain.invoke({'article':article, 'ai_result':ai_result})
+
+        return result
+
+    def reflection_batch(self, batch_list: list):
+        reflection_prompt = PromptTemplate.from_template(REFLECTION_TEST)
+        chain = reflection_prompt | self.client | StrOutputParser()
+        result = chain.batch(batch_list)
+
+        return result
+
+    def summarize_chain(self, article: str):
+        chain_summarize_prompt = PromptTemplate.from_template(SUMMARY_CHAIN_PROMPT)
+        chain = chain_summarize_prompt | self.client | StrOutputParser()
+        result = chain.invoke({'article':article})
+
+        return result
+
+    def summarize_chain_batch(self, batch_list: list):
+        chain_summarize_prompt = PromptTemplate.from_template(SUMMARY_CHAIN_PROMPT)
+        chain = chain_summarize_prompt | self.client | StrOutputParser()
+        result = chain.batch(batch_list)
 
         return result
